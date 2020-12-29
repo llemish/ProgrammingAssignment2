9a4975f608c410988a961e11383b635692dd75c8
Levshinovskiy Mikhail <llemish@gmail.com>
2020-12-29 14:39:53 +0300

    Create functions for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a special matrix, that is list of function for working wish
+## cached matrix result, and another function that count inverse matrix
 
-## Write a short comment describing this function
+## Create a "special" matrix, that is really list of function for work with
+## cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y){
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Count inverse matrix for given, use cache if already computed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)){
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
